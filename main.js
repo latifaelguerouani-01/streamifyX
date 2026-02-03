f882fc4bb79a1af2d7be4397ffa9f8855b5601df
latifaelguerouani-01 <latifaelguerouani@gmail.com>
2026-02-03 14:55:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,84 +1,58 @@
-let menu = document.querySelector('#menu-bars');
-let navbar = document.querySelector('.navbar');
+window.addEventListener("pageshow", () => {
 
-menu.onclick = () =>{
-  menu.classList.toggle('fa-times');
-  navbar.classList.toggle('active');
-}
+  const menu = document.querySelector('#menu-bars');
+  const navbar = document.querySelector('.navbar');
 
+  if (menu && navbar) {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 
-var swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 7500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop:true,
+    menu.onclick = () => {
+      menu.classList.toggle('fa-times');
+      navbar.classList.toggle('active');
+    };
+  }
+
+  const homeSlider = document.querySelector(".home-slider");
+  if (homeSlider) {
+    if (homeSlider.swiper) homeSlider.swiper.destroy(true, true);
+
+    new Swiper(".home-slider", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop: true,
+    });
+  }
+
+  document.querySelectorAll(
+    ".anime-slider, .action-slider, .child-slider, .family-slider"
+  ).forEach(slider => {
+
+    if (slider.swiper) slider.swiper.destroy(true, true);
+
+    new Swiper(slider, {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      loop: true,
+      breakpoints: {
+        0: { slidesPerView: 1 },
+        768: { slidesPerView: 2 },
+        1024: { slidesPerView: 4 },
+      }
+    });
   });
 
-  var swiper = new Swiper(".anime-slider", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 4500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop:true
-  });
-
-
-  var swiper = new Swiper(".action-slider", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 3500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop:true
-  });
-
-
-  var swiper = new Swiper(".child-slider", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop:true
-  });
-
-  var swiper = new Swiper(".family-slider", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 1500,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    loop:true
-  });
+});
+window.openMovie = function (movieName) { window.location.href = "download.html?movie=" + movieName; };
