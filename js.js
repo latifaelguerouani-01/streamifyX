17285647c42f98985ab9caf95746edb8c5054a0b
latifaelguerouani-01 <latifaelguerouani@gmail.com>
2026-05-07 20:21:16 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-const allStar = document.querySelectorAll('.rating .star')const allStar = document.querySelectorAll('.rating .star')
+const allStar = document.querySelectorAll('.rating .star')
 const ratingValue = document.querySelector('.rating input')
 
 allStar.forEach((item, idx)=> {
@@ -437,27 +437,3 @@ if (dateEl) dateEl.innerText = movie.date;
 if (titleEl) titleEl.innerText = movie.title;
 if (typeEl) typeEl.innerText = movie.type;
 if (studioEl) studioEl.innerText = movie.studio;
-
-const ratingValue = document.querySelector('.rating input')
-
-allStar.forEach((item, idx)=> {
-	item.addEventListener('click', function () {
-		let click = 0
-		ratingValue.value = idx + 1
-
-		allStar.forEach(i=> {
-			i.classList.replace('bxs-star', 'bx-star')
-			i.classList.remove('active')
-		})
-		for(let i=0; i<allStar.length; i++) {
-			if(i <= idx) {
-				allStar[i].classList.replace('bx-star', 'bxs-star')
-				allStar[i].classList.add('active')
-			} else {
-				allStar[i].style.setProperty('--i', click)
-				click++
-			}
-		}
-	})
-
-})
